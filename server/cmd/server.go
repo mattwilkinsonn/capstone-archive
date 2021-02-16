3d8333ac392839869d73ecc64624dec452e91374
Matt Wilkinson <mattwilki17@gmail.com>
2021-02-16 16:01:01 -0500

    clean up server code into helpers

--- server/cmd/server.go
+++ server/cmd/server.go
@@ -1,28 +1,25 @@
 package main
 
 import (
-	"os"
-
 	"github.com/Zireael13/capstone-archive/server/internal/db"
+	"github.com/Zireael13/capstone-archive/server/internal/helpers"
 	"github.com/Zireael13/capstone-archive/server/internal/routes"
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 	"github.com/matthewhartstonge/argon2"
 )
 
-const defaultPort = "8080"
-
 func main() {
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = defaultPort
-	}
+	_ = godotenv.Load(".env")
+
+	port := helpers.GetPort()
 
 	orm := db.CreateDatabaseClient()
 	argon := argon2.DefaultConfig()
 
 	g := gin.Default()
 
-	g.POST("/graphql", routes.GraphQLHandler(orm, &argon))
-	g.GET("/", routes.PlaygroundHandler())
-	g.Run("localhost:4000")
+	routes.AttachRoutes(g, orm, &argon)
+
+	helpers.RunServer(g, port)
 }
---
+++ server/internal/helpers/serverHelpers.go
@@ -0,0 +1,25 @@
+package helpers
+
+import (
+	"os"
+
+	"github.com/gin-gonic/gin"
+)
+
+const defaultPort = "4000"
+
+func GetPort() (port string) {
+	port = os.Getenv("PORT")
+	if port == "" {
+		port = defaultPort
+	}
+
+	return
+}
+
+func RunServer(g *gin.Engine, port string) {
+	err := g.Run("localhost:" + port)
+	if err != nil {
+		panic(err)
+	}
+}
