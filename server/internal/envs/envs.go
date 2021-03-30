81e3b3c7b33babf63cd9eac1ad745f145f7dddb4
Matt Wilkinson <mattwilki17@gmail.com>
2021-03-29 21:32:21 -0400

    envs

--- server/internal/envs/envs.go
+++ server/internal/envs/envs.go
@@ -1,6 +1,30 @@
 package envs
 
-import "os"
+import (
+	"fmt"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+func LoadEnvs() {
+
+	errLocal := godotenv.Load(".env.local")
+	err := godotenv.Load(".env")
+
+	if errLocal != nil && err != nil {
+
+		fmt.Printf("No .env files found\n")
+
+		fmt.Printf("%v\n%v\n", errLocal, err)
+
+		if err != nil {
+			panic(err)
+		}
+
+		panic(errLocal)
+	}
+}
 
 const defaultPort = "4000"
 
