ad80c6afa1cc5ac026dba15e86151bd06a148a21
Matt Wilkinson <mattwilki17@gmail.com>
2021-08-24 17:49:08 -0400

    add demo logout rpc

--- auth/src/server.rs
+++ auth/src/server.rs
@@ -2,7 +2,7 @@ use tonic::{transport::Server, Request, Response, Status};
 
 use auth_proto::auth_server::{Auth, AuthServer};
 
-use auth_proto::{LoginReply, LoginRequest, Role, User};
+use auth_proto::{LoginReply, LoginRequest, LogoutReply, Role, User};
 
 pub mod auth_proto {
     tonic::include_proto!("auth"); // The string specified here must match the proto package name
@@ -13,10 +13,7 @@ pub struct Authenticator {}
 
 #[tonic::async_trait]
 impl Auth for Authenticator {
-    async fn login(
-        &self,
-        _request: Request<LoginRequest>,
-    ) -> Result<tonic::Response<LoginReply>, Status> {
+    async fn login(&self, _request: Request<LoginRequest>) -> Result<Response<LoginReply>, Status> {
         let user = User {
             id: "1".to_string(),
             created_at: "10/21/2021".to_string(),
@@ -28,6 +25,12 @@ impl Auth for Authenticator {
         };
 
         Ok(Response::new(LoginReply { user: Some(user) }))
+    }
+
+    async fn logout(&self, _request: Request<()>) -> Result<Response<LogoutReply>, Status> {
+        let logout = LogoutReply { logout: true };
+
+        Ok(Response::new(logout))
     }
 }
 
